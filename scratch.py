55bb6dbee56ad83e3cf1c8b97186fdfaa78f537b
spbokn <bokn>
2018-06-14 10:30:51 +0300

    filtering results

--- scratch.py
+++ scratch.py
@@ -1,5 +1,4 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
-import json
 
 query = """
 SELECT ?property ?hasValue ?isValueOf
@@ -9,10 +8,18 @@ WHERE {
   { ?isValueOf ?property <http://dbpedia.org/resource/Toyota> }
 }
 """
-sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-sparql.setQuery(query)
-sparql.setReturnFormat(JSON)
-results = sparql.query().convert()
-print(results['results']['bindings'])
-#parsed = json.loads(str(results))
-#print(json.dumps(parsed, indent=4, sort_keys=True))
+with open('kek.txt', 'w') as f:
+    _not_tup_prop = ('sameAs', 'rdf-schema#label', 'rdf-schema#comment', 'logo', 'image', 'property/symbol', '#seeAlso'\
+                , 'wikiPageID', 'wikiPageRevisionID', 'thumbnail', 'http://xmlns.com/', '/property/name', \
+                '/property/secCik', '/ontology/abstract', 'prov#wasDerivedFrom', 'property/nativeName', \
+                     'property/titlestyle', 'property/width')
+    _not_tup_hasV = ('owl#Thing',)
+    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    for r in results['results']['bindings']:
+        if not r.get('isValueOf'):
+            if not any(t in r['property']['value'] for t in _not_tup_prop) \
+                    and not any(t in r['hasValue']['value'] for t in _not_tup_hasV):
+                f.write('PROP: ' + str(r['property']) + ' || ' + 'VALUE: ' + str(r['hasValue']) + '\n')
